6cec1d56be0c15c2071f053e7fe53588b6c55246
ubuntu <ubuntu@ubuntu.com>
2017-08-26 00:20:54 +0900

    pdf1.py is an example of able to cut several pages from a pdf

--- pdf1.py
+++ pdf1.py
@@ -1,38 +1,72 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 from PyPDF2 import PdfFileWriter, PdfFileReader
-import  PyPDF2
-import re
-import string
-placeholder=""
 
-p=re.compile('.*CHAPTER\s\d{1,2}.*')
-q=re.compile('.*Switch Port Configuration.*')
+inputpdf = PdfFileReader(open('1.pdf', 'rb'))
+if inputpdf.isEncrypted:
+    inputpdf.decrypt("")
 
-pdfFileObj = open('1.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-number = pdfReader.numPages
-#for i in range( 32,34):
-pageObj = pdfReader.getPage(59)
-placeholder += pageObj.extractText()
-lines = placeholder.split('\n')
-#~ for line in lines:
-    #~ m = p.match(line)
-    #~ if m!=None:
-        #~ print (lines[0], lines[1], lines[2], lines[3])
-        #~ print ("//////////////////////////////////////")
-        #~ print (lines.index(line))
-        #~ print ("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-print (lines)
-placeholder=""
+#inputpdf = PdfFileReader(open("1.pdf", "rb"))
+#inputpdf.numPages-1
+output = PdfFileWriter()
+output.addPage(inputpdf.getPage(5))
+output.addPage(inputpdf.getPage(6))
+output.addPage(inputpdf.getPage(7))
+with open("hello.pdf", "wb") as outputStream:
+    output.write(outputStream)
+
+#~ from PyPDF2 import PdfFileWriter, PdfFileReader
+#~ import  PyPDF2
+#~ import re
+#~ import string
+#~ placeholder=""
+
+#~ pdfReader = PdfFileReader(open("2.pdf", 'rb'))
+#~ if pdfReader.isEncrypted:
+    #~ pdfReader.decrypt("")
+    #~ output = PdfFileWriter()
+    #~ output.addPage(pdfReader.getPage(0))
+    #~ output.addPage(pdfReader.getPage(1))
+    #~ with open("lala.pdf", "wb") as outfp:
+        #~ output.write(outfp)
+    
     
 
-#~ for i in range(inputpdf.numPages):
+#~ p=re.compile('.*CHAPTER\s\d{1,2}.*')
+#~ locations = []
+
+#~ def makePdf( title, beg, end):
     #~ output = PdfFileWriter()
-    #~ output.addPage(inputpdf.getPage(i))
-    #~ with open("document-page%s.pdf" % i, "wb") as outputStream:
-        #~ output.write(outputStream)
+    #~ print ("beg: ", beg)
+    #~ print ("end: ", end)
+    #~ title=title.strip()
+    #~ print (title)
+    #~ for i in range(beg, end-1): 
+        #~ output.addPage(pdfReader.getPage(i))
+        #~ print (i)
+    
+    #~ with open(title+".pdf", "wb") as outfp:
+        #~ output.write(outfp)
+    #~ output=None
 
+#~ number = pdfReader.numPages
+#~ for i in range( 31, number):
+    #~ pageObj = pdfReader.getPage(i)
+    #~ placeholder += pageObj.extractText()
+    #~ lines = placeholder.split('\n')
+    #~ for line in lines:
+        #~ m = p.match(line)
+        #~ if m!=None:
+            #~ chapterTitle=lines[ len(lines)-2] 
+            #~ locations.append([chapterTitle, i])
 
+    #~ placeholder=""
+#~ for i in range(0, len(locations)-1):
+    #~ title=locations[i][0]
+    #~ beg=locations[i][1]
+    #~ try:
+        #~ end=locations[i+1][1]
+    #~ except:
+        #~ end=507
+    #~ makePdf( title, beg, end)
